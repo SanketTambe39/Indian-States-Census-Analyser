da0c02df0265e9286921a49fd891bd70feea22e4
SanketTambe39 <sankettambe39@gmail.com>
2021-07-23 15:11:42 +0530

    ADDED: Test case for throwing exception if wrong file is given

--- Indian-States-Census-Analyser.git/src/main/java/censusanalyser/CensusAnalyserException.java
+++ Indian-States-Census-Analyser.git/src/main/java/censusanalyser/CensusAnalyserException.java
@@ -2,11 +2,11 @@ package censusanalyser;
 
 public class CensusAnalyserException extends Exception {
 
-	 enum ExceptionType {
+	 public enum ExceptionType {
 	        WRONG_FILE_TYPE, NO_SUCH_FILE, NO_SUCH_FIELD
 	    }
 
-	    ExceptionType exceptionType;
+	    public ExceptionType exceptionType;
 		public CensusAnalyserException(String message,ExceptionType exceptionType) {
 			super();
 			this.exceptionType = exceptionType;
--- Indian-States-Census-Analyser.git/src/test/java/censusanalysertest/CensusAnalyserTest.java
+++ Indian-States-Census-Analyser.git/src/test/java/censusanalysertest/CensusAnalyserTest.java
@@ -2,17 +2,19 @@ package censusanalysertest;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import censusanalyser.CensusAnalyser;
 import censusanalyser.CensusAnalyserException;
 
+
 public class CensusAnalyserTest {
 
 	private static final String INDIA_CENSUS_CSV_FILE_PATH = "C:\\Users\\Sanket\\Desktop\\StateCensusData.csv";
-	private static final String WRONG_CSV_FILE_PATH = "C:\\Users\\Sanket\\Downloads\\StateCensusData.csv";
+	private static final String WRONG_CSV_FILE_PATH = "C:\\Users\\Sanket\\Downloads\\StateCensusData1.csv";
 	private static final String WRONG_CSV_FILE_TYPE = "C:\\Users\\Sanket\\Desktop\\StateCensusData.pdf";
 	private static final String WRONG_DELIMITER = "C:\\Users\\Sanket\\Desktop\\StateCensusData.csv";
-	private static final String NO_HEADER = "C:\\Users\\Shravya\\Sanket\\StateCensusData.csv";
+	private static final String NO_HEADER = "C:\\Users\\Sanket\\StateCensusData.csv";
 
 	@Test
     public void givenIndianCensusCSV_WhenFileReturnsCorrectNumberOfRecords_ShouldReturnNumberOfRecords() {
@@ -22,6 +24,17 @@ public class CensusAnalyserTest {
             Assert.assertEquals(29, numberOfRecords);
         } catch(CensusAnalyserException ignored) {
         	
+        }
+    }
+	@Test
+    public void givenIndianCensusData_WhenWrongFile_ShouldThrowException() {
+        try {
+            CensusAnalyser censusAnalyser = new CensusAnalyser();
+            ExpectedException expectedException = ExpectedException.none();
+            expectedException.expect(CensusAnalyserException.class);
+            censusAnalyser.loadIndiaCensusData(WRONG_CSV_FILE_PATH);
+        } catch(CensusAnalyserException censusAnalyserException) {
+            Assert.assertEquals(CensusAnalyserException.ExceptionType.NO_SUCH_FILE, censusAnalyserException.exceptionType);
         }
     }
 	
