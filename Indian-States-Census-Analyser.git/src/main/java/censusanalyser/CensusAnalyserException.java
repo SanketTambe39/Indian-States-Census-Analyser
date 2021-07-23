bfcbd164e8314ac04af85a805b13acfe3908a68e
SanketTambe39 <sankettambe39@gmail.com>
2021-07-23 14:45:11 +0530

    ADDED: Method to load data from csv and exception handling

--- Indian-States-Census-Analyser.git/src/main/java/censusanalyser/CensusAnalyserException.java
+++ Indian-States-Census-Analyser.git/src/main/java/censusanalyser/CensusAnalyserException.java
@@ -1,5 +1,14 @@
 package censusanalyser;
 
-public class CensusAnalyserException {
+public class CensusAnalyserException extends Exception {
 
+	 enum ExceptionType {
+	        WRONG_FILE_TYPE, NO_SUCH_FILE, NO_SUCH_FIELD
+	    }
+
+	    ExceptionType exceptionType;
+		public CensusAnalyserException(String message,ExceptionType exceptionType) {
+			super();
+			this.exceptionType = exceptionType;
+		}	
 }
--- Indian-States-Census-Analyser.git/src/main/java/censusanalyser/IndianCensusCSV.java
+++ Indian-States-Census-Analyser.git/src/main/java/censusanalyser/IndianCensusCSV.java
@@ -1,5 +1,16 @@
 package censusanalyser;
 
+import com.opencsv.bean.CsvBindByName;
+
 public class IndianCensusCSV {
+
+	@CsvBindByName(column = "State")
+    public String state;
+    @CsvBindByName(column = "Population")
+    public long population;
+    @CsvBindByName(column = "AreaInSqKm" )
+    public long areaInSqKm;
+    @CsvBindByName(column = "DensityPerSqKm")
+    public long densityPerSqKm;
 
 }
